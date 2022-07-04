89b56d2ea1d2daea587132d9bc505da1e36a8cec
Rudy Ferraz <108693578+Rudyzone@users.noreply.github.com>
2022-07-04 12:23:40 -0700

    Add files via upload

---
+++ src/get-metadata.js
@@ -0,0 +1,35 @@
+// Note: Please do not use JSDOM or any other external library/package (sorry)
+/*
+type Metadata = {
+  url: string;
+  siteName: string;
+  title: string;
+  description: string;
+  keywords: string[];
+  author: string;
+};
+*/
+
+/**
+ * Gets the URL, site name, title, description, keywords, and author info out of the <head> meta tags from a given html string.
+ * 1. Get the URL from the <meta property="og:url"> tag.
+ * 2. Get the site name from the <meta property="og:site_name"> tag.
+ * 3. Get the title from the the <title> tag.
+ * 4. Get the description from the <meta property="og:description"> tag or the <meta name="description"> tag.
+ * 5. Get the keywords from the <meta name="keywords"> tag and split them into an array.
+ * 6. Get the author from the <meta name="author"> tag.
+ * If any of the above tags are missing or if the values are empty, then the corresponding value will be null.
+ * @param html The complete HTML document text to parse
+ * @returns A Metadata object with data from the HTML <head>
+ */
+export default function getMetadata(html) {
+  // TODO: delete and replace this with your code
+  return {
+    url: "https://www.example.com/something/to-test",
+    siteName: "Example Site Name",
+    title: "Example Title",
+    description: "Example description.",
+    keywords: ["example", "keywords"],
+    author: "Example Author",
+  };
+}
